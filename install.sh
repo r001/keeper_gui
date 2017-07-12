83526c9bb4fe1e3e45564c26067d4e0e74da138b
r001 <rob@nyar.eu>
2017-07-12 10:54:25 +0200

    added install.sh for debian based systems

---
+++ install.sh
@@ -0,0 +1,8 @@
+git submodule update --init --recursive
+sudo apt-get install python3.6 gcc python3.6-dev python3.6-venv python3-pip
+python3.6 -m pip install --upgrade pip
+python3.6 -m pip install --upgrade venv
+python3.6 -m venv flask
+flask/bin/pip install --upgrade pip	
+flask/bin/pip install flask
+flask/bin/pip install flask-wtf
